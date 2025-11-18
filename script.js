2a9b477d2c968339c3c0308bcb38e19122d824f1
JatinKumarGupta-png <jatinbansagar117@gmail.com>
2025-11-18 19:34:17 +0530

    crrate the random div

--- script.js
+++ script.js
@@ -1,101 +1,50 @@
-
-let arr = [{
-        team: "CSK",
-        primary : "Yellow",
-        Secondary: "Blue",
-        FullName: "Chennai Super Kings",
-        trophies: 4,
-        captain: "MS Dhoni",
-    },  
-    {
-        team: "MI",
-        primary : "orange",
-        Secondary: "dark red",
-        FullName: "Mumbai Indians",
-        trophies: 5,
-        captain: "Rohit Sharma",
-    },
-    {
-        team: "RCB",
-        primary : "Red",
-        Secondary: "silver",     
-        FullName: "Royal Challengers Bangalore",
-        trophies: 0,
-        captain: "Faf Du Plessis",
-    },
-    {
-        team: "KKR",
-        primary : "Purple",
-        Secondary: "navy",      
-        FullName: "Kolkata Knight Riders",      
-        trophies: 2,
-        captain: "Shreyas Iyer",
-
-    },{
-        team: "SRH",
-        primary : "Orange",
-        Secondary: "Grey",      
-        FullName: "Sunrisers Hyderabad",      
-        trophies: 1,
-        captain: "Aiden Markram",
-    },
-    {
-        team: "DC",
-        primary : "green",
-        Secondary: "Red",
-        FullName: "Delhi Capitals",
-        trophies: 0,
-        captain: "Rishabh Pant",
-    },                  
-    {
-        team: "PBKS",
-        primary : "black",
-        Secondary: "crimson",       
-        FullName: "Punjab Kings",       
-        trophies: 0,
-        captain: "Shikhar Dhawan",
-    },
-    {
-        team: "RR",
-        primary : "blue",
-        Secondary: "pink",      
-        FullName: "Rajasthan Royals",      
-        trophies: 1,
-        captain: "Sanju Samson",
-    },
-    {
-        team: "LSG",
-        primary : "Yellow",
-        Secondary: " Blue",
-        FullName: "Lucknow Super Giants",
-        trophies: 0,
-        captain: "K.L Rahul",
-    },
-    {
-        team: "GT",
-        primary : "Blue",
-        Secondary: "Silver",
-        FullName: "Gujarat Titans",
-        trophies: 1,
-        captain: "Hardik Pandya",
-    }                              
-]
-
-let body = document.querySelector("body");
-let h1 = document.querySelector("h1");
-let btn = document.querySelector(".btn");
-
-btn.addEventListener("click", function(){
-        let winner = arr[Math.floor(Math.random() * arr.length)]
-        h1.innerHTML = winner.team
-        h1.innerHTML += `<p>Full Name: ${winner.FullName}</p>`
-        h1.innerHTML += `<p>Trophies Won: ${winner.trophies}</p>`
-        h1.innerHTML += `<p>Captain: ${winner.captain}</p>`
-        h1.style.backgroundColor = winner.Secondary
-       
-        body.style.backgroundColor = winner.primary
-
-})
+const main = document.querySelector('main')
+const btn = document.querySelector('button')
+const image =['img1.jpg', 
+    'https://images.unsplash.com/photo-1621129171751-9604ed97963f?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 
+    'https://plus.unsplash.com/premium_photo-1673967831980-1d377baaded2?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 
+    'https://plus.unsplash.com/premium_photo-1664371674830-aa12607fd1ab?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 
+    'https://plus.unsplash.com/premium_photo-1664371675060-87e49122263c?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://plus.unsplash.com/premium_photo-1694198817869-196127afd4ef?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',]
+    main.addEventListener('click',function(){
+        const img = document.createElement('img')
+        let imgIndex = Math.floor(Math.random()*image.length)
+        img.style.height = '70px'
+        img.style.width ='70px'
+        img.style.position = 'absolute'
+        let x = Math.floor(Math.random()*90)
+        let y = Math.floor(Math.random()*90)
+        let rotate = Math.floor (Math.random()*360)
+        let size = Math.floor (Math.random()*100)+20
+        img.style.top = x+'%'
+        img.style.left = y+'%'
+        img.style.transform = `rotate(${rotate}deg)`
+        
+        img.style.objectFit = 'cover'
+        img.style.objectPosition = 'center'
+        img.src = image[imgIndex]
+        main.appendChild (img)
+    })
+// btn.addEventListener('click',function(){
+//     const div = document.createElement('div')
+//     div.style.height = '100px'
+//     div.style.width ='100px'
+//     div.style.backgroundColor = 'red'
+//     let x = Math.floor(Math.random()*80)
+//     let y = Math.floor(Math.random()*80)
+//     let rotate = Math.floor (Math.random()*360)
+//     let c1 = Math.floor (Math.random()*256)
+//     let c2 = Math.floor (Math.random()*256)
+//     let c3 = Math.floor (Math.random()*256)
+//     div.style.position = 'absolute'
+   
+//     div.style.top = x+'%'
+//     div.style.left = y+'%'
+//     div.style.transform = 'rotate('+ rotate +'deg)'
+//     div.style.backgroundColor = `rgb(${c1},${c2},${c3})`
+//     main.appendChild (div)
+    
+// })
       
 
 
