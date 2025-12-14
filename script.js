b16bd32064e17853570d0cbdf5fce73901bfdaba
JatinKumarGupta-png <jatinbansagar117@gmail.com>
2025-12-14 06:43:51 +0530

    instagram like

--- script.js
+++ script.js
@@ -1,42 +1,17 @@
-var grow = 0
-var btn = document.querySelector('button')
-var h2 = document.querySelector('h2')
-var inner = document.querySelector('.inner')
-btn.addEventListener('click',function(){
-    btn.style.pointerEvents = 'none'
-    var num = 50+Math.floor(Math.random()*50)
-    
-    
-    var int =setInterval(()=>{
-        grow++
-        h2.innerHTML = grow+'%'
-        inner.style.width =grow+'%'
-    },num)
-    setTimeout(()=>{
-        clearInterval(int)
-        btn.style.opacity = 0.5
-        btn.innerHTML = 'Downloaded'
-        console.log('Downloaded in',num/10,'soconds');
-    },num*100)
-    
-})
-// var grow = 0
-// const btn = document.querySelector('button')
-// const h2 = document.querySelector('h2')
-// const inner = document.querySelector('.inner')
-// btn.addEventListener('click',function(){
-//     btn.style.pointerEvents = 'none'
-//     let num = 50+Math.floor(Math.random()*50)
-     
-//     var int = setInterval(() =>{
-//         grow++
-//         h2.innerHTML = grow+'%'
-//         inner.style.width = grow+'%'
-//     },num)
-//     setTimeout(() =>{
-//         clearInterval(int)
-//         btn.innerHTML = 'Downloaded'
-//         btn.style.opacity = 0.5
-//         console.log('Downloaded in',num/10,'seconds' )
-//     },num*100)
-// })
+var img = document.querySelector('img');
+var love = document.querySelector('#love');
+
+img.addEventListener('click', function(){
+    love.style.opacity = 1;
+    love.style.transform = 'translate(-50%, -50%) scale(1) rotate(0deg)';
+    setTimeout(function(){
+        love.style.transform = 'translate(-50%, -500%) scale(1) rotate(0deg)';
+        
+    },800);
+    setTimeout(function(){
+        love.style.opacity = 0;
+    },1000);
+    setTimeout(function(){
+        love.style.transform = 'translate(-50%, -50%) scale(0) rotate(-60deg)';
+    },1200);
+});
